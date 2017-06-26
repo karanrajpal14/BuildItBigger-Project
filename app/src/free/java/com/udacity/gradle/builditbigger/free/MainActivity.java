68b78c8496d9b16a33f51f6f189990bfbd8c7a41
karanrajpal14 <rajpal.karan73@gmail.com>
2017-06-26 14:08:45 +0530

    Added Interstitial Ad

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -2,23 +2,78 @@ package com.udacity.gradle.builditbigger.free;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 import com.udacity.gradle.builditbigger.BuildConfig;
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String LOG_TAG = "FreeDebug";
+    PublisherInterstitialAd publisherInterstitialAd = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (BuildConfig.ADS) {
+
+            //Set up for pre-fetching interstitial ad request
+            publisherInterstitialAd = new PublisherInterstitialAd(getApplicationContext());
+            publisherInterstitialAd.setAdUnitId("/6499/example/interstitial");
+
+            publisherInterstitialAd.setAdListener(new AdListener() {
+                @Override
+                public void onAdClosed() {
+                    super.onAdClosed();
+                    tellJoke();
+                    //Prefetch next ad
+                    requestNewInterstitial();
+                }
+
+                @Override
+                public void onAdFailedToLoad(int i) {
+                    super.onAdFailedToLoad(i);
+                    Log.d(LOG_TAG, "onAdFailedToLoad(): Failed to load ad with error code: " + i);
+
+                    //Prefetch next ad
+                    requestNewInterstitial();
+                }
+
+                @Override
+                public void onAdLoaded() {
+                    Log.d(LOG_TAG, "onAdLoaded(): Ad loaded successfully");
+                    super.onAdLoaded();
+                }
+            });
+
+            requestNewInterstitial();
+
+            Button tellJokeButton = (Button) findViewById(R.id.tell_joke_button);
+            tellJokeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (publisherInterstitialAd.isLoaded()) {
+                        Log.d(LOG_TAG, "onClick(): Interstatial ad ready");
+                        publisherInterstitialAd.show();
+                    } else {
+                        Log.d(LOG_TAG, "onClick(): Interstatial ad was not ready");
+                        tellJoke();
+                    }
+                }
+            });
+
             AdView mAdView = (AdView) findViewById(R.id.adView);
             // Create an ad request. Check logcat output for the hashed device ID to
             // get test ads on a physical device. e.g.
@@ -28,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                     .build();
             mAdView.loadAd(adRequest);
         }
+
     }
 
     @Override
@@ -52,8 +108,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view) {
+    public void tellJoke() {
         new EndpointsAsyncTask().execute(this);
+    }
+
+    private void requestNewInterstitial() {
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                //.addTestDevice("EA27D37DF5448BF42AA5F7A6D4F11A9B")
+                .build();
+
+        publisherInterstitialAd.loadAd(adRequest);
     }
 
 }
